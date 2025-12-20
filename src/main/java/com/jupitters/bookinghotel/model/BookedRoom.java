c8d40ca02bb720e4ee5d45b49d514742ec2c23e9
jupitters <sampaiobale@gmail.com>
2025-12-20 20:50:46 -0300

    booked room model

--- src/main/java/com/jupitters/bookinghotel/model/BookedRoom.java
+++ src/main/java/com/jupitters/bookinghotel/model/BookedRoom.java
@@ -1,6 +1,6 @@
 package com.jupitters.bookinghotel.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +8,34 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+    private Long id;
+    private LocalDate checkinDate;
+    private LocalDate checkoutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuests;
+    private String bookingConfirmationCode;
+    private Room room;
 
+    public void calculateTotalGuests(){
+        this.totalNumOfGuests = this.numOfAdults + this.numOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalGuests();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalGuests();
+    }
 }
