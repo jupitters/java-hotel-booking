4d32c67e31c8d02ec49a119f05db27b85c403027
jupitters <sampaiobale@gmail.com>
2025-12-20 20:53:45 -0300

    room model

--- src/main/java/com/jupitters/bookinghotel/model/Room.java
+++ src/main/java/com/jupitters/bookinghotel/model/Room.java
@@ -1,14 +1,28 @@
 package com.jupitters.bookinghotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String roomType;
     private BigDecimal roomPrice;
     private boolean isBooked;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<BookedRoom> bookings;
 
     public Room(){
