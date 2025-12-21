90084ea6c168f9319c3085aa74525466f4d0f5f5
jupitters <sampaiobale@gmail.com>
2025-12-21 20:00:35 -0300

    booked room dto

--- src/main/java/com/jupitters/bookinghotel/dto/BookedRoomDto.java
+++ src/main/java/com/jupitters/bookinghotel/dto/BookedRoomDto.java
@@ -1,7 +1,14 @@
 package com.jupitters.bookinghotel.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoomDto {
     private Long id;
     private LocalDate checkinDate;
