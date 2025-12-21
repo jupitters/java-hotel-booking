d6b8fd137d1459217f998438f78e4eaeb300c148
jupitters <sampaiobale@gmail.com>
2025-12-21 19:55:45 -0300

    room dto

--- src/main/java/com/jupitters/bookinghotel/dto/RoomDto.java
+++ src/main/java/com/jupitters/bookinghotel/dto/RoomDto.java
@@ -1,8 +1,14 @@
 package com.jupitters.bookinghotel.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
+import java.util.Base64;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
 public class RoomDto {
     private Long id;
     private String roomType;
@@ -10,4 +16,13 @@ public class RoomDto {
     private boolean isBooked;
     private String photo;
     private List<BookedRoomDto> bookings;
+
+    public RoomDto(Long id, String roomType, BigDecimal roomPrice, boolean isBooked, byte[] photoByte, List<BookedRoomDto> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.isBooked = isBooked;
+        this.photo = photoByte != null ? Base64.getEncoder().encodeToString(photoByte) : null;
+        this.bookings = bookings;
+    }
 }
