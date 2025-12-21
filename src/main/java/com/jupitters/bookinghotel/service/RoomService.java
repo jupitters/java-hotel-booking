22387c2f561975bd78773690bf78ce7e62ae31e0
jupitters <sampaiobale@gmail.com>
2025-12-21 20:24:41 -0300

    room service impl

--- src/main/java/com/jupitters/bookinghotel/service/RoomService.java
+++ src/main/java/com/jupitters/bookinghotel/service/RoomService.java
@@ -1,4 +1,10 @@
 package com.jupitters.bookinghotel.service;
 
+import com.jupitters.bookinghotel.model.Room;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
 public interface RoomService {
+    Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice);
 }
--- src/main/java/com/jupitters/bookinghotel/service/impl/RoomServiceImpl.java
+++ src/main/java/com/jupitters/bookinghotel/service/impl/RoomServiceImpl.java
@@ -1,4 +1,19 @@
 package com.jupitters.bookinghotel.service.impl;
 
-public class RoomServiceImpl {
+import com.jupitters.bookinghotel.model.Room;
+import com.jupitters.bookinghotel.repository.RoomRepository;
+import com.jupitters.bookinghotel.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
+public class RoomServiceImpl implements RoomService {
+    private final RoomRepository roomRepository;
+
+    @Override
+    public Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice) {
+
+    }
 }
