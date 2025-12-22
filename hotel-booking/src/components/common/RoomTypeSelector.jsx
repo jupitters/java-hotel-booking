207cab7f6acad1f673a66339cbeae5357e6da313
jupitters <sampaiobale@gmail.com>
2025-12-21 22:16:52 -0300

    room type selector component

--- hotel-booking/src/components/common/RoomTypeSelector.jsx
+++ hotel-booking/src/components/common/RoomTypeSelector.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {getRoomTypes} from '../utils/ApiFunctions'
 
-const RoomTypeSelector = () => {
-  return (
-    <div>RoomTypeSelector</div>
-  )
+const RoomTypeSelector = ({handleRoomInputChange, newRoom}) => {
+    const [roomTypes, setRoomTypes] = useState([])
+    const [showNewRoomTypesInput, setShowNewRoomTypesInput] = useState(false)
+    const [newRoomType, setNewRoomType] = useState("")
+
+    useEffect(() => {
+        getRoomTypes().then((data) => {
+            setRoomTypes(data)
+        })
+    }, [])
+
+    const handleNewRoomInputChange = (e) => {
+        setNewRoomType(e.target.value)
+    }
+
+    const handleAddNewRoomType = () => {
+        if(newRoomType !== "") {
+            setRoomTypes([...roomTypes, newRoomType])
+            setNewRoomType("")
+            setShowNewRoomTypesInput(false)
+        }
+    }
+
+    return (
+        <>
+        {roomTypes.length > 0 && (
+            <div>
+            <select id="roomType" name='roomType' value={newRoom.roomType}>
+
+            </select>
+            </div>
+        )}
+        </>
+    )
 }
 
 export default RoomTypeSelector
