f4f953468ec4ec23de462cb6d381116d9a0f46f7
jupitters <sampaiobale@gmail.com>
2025-12-21 21:55:10 -0300

    add rom component form

--- hotel-booking/src/components/room/AddRoom.jsx
+++ hotel-booking/src/components/room/AddRoom.jsx
@@ -34,16 +34,36 @@ const AddRoom = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            const success = await AddRoom(newRoom.photo, newRoom.roomType, newRoom.roomPrice)
+            const success = await addRoom(newRoom.photo, newRoom.roomType, newRoom.roomPrice)
+            if(success != undefined){
+                setSuccsessMessage("A new room was added to the database!")
+                setNewRoom({photo: null, roomType: "", roomPrice: ""})
+                setImagePreview("")
+                setErrorMessage("")
+            } else {
+                setErrorMessage("Error adding room!")
+            }
         } catch (error) {
             setErrorMessage(error.message)
         }
     }
 
     return (
-        <div>
+        <>
+        <section className='container mt-5 mb-5'>
+            <div className='row justify-content-center'>
+                <div className='col-md-8 col-lg-6'>
+                    <h2 className='mt-5 mb-2'>Add New Room</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className='mb-3'>
+                            <label htmlFor="roomType" className='form-label'>Room Type</label>
 
-        </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+        </>
     )
 }
 
