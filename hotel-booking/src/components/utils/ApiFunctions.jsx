9c1bfd66261c5c12b452683ae3b5fbe8a00ad94c
jupitters <sampaiobale@gmail.com>
2025-12-21 21:29:45 -0300

    api functions

--- hotel-booking/src/components/utils/ApiFunctions.jsx
+++ hotel-booking/src/components/utils/ApiFunctions.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import axios from 'axios';
 
-const ApiFunctions = () => {
-  return (
-    <div>ApiFunctions</div>
-  )
+const api = axios.create({
+    baseURL: "http://localhost:9192"
+})
+
+async function addRoom(photo, roomType, roomPrice){
+    const formData = new FormData()
+
+    formData.append("photo", photo)
+    formData.append("roomType", roomType)
+    formData.append("roomPrice", roomPrice)
+
+    const response = await api.post("/room/add", formData)
+    if(response.status === 201){
+        return true
+    } else {
+        return false
+    }
 }
 
-export default ApiFunctions
+async function getRoomTypes(){
+    try{
+        const response = await axios.get("/room/room-types")
+        return response.data
+    } catch(e) {
+        throw new Error("Error fetching room types!")
+    }
+}
+
+export default {api, addRoom, getRoomTypes}
