cc73f5370e9c4f05ecb10539efa8b138bb3793be
jupitters <sampaiobale@gmail.com>
2025-12-22 20:33:30 -0300

    room repository

--- src/main/java/com/jupitters/bookinghotel/repository/RoomRepository.java
+++ src/main/java/com/jupitters/bookinghotel/repository/RoomRepository.java
@@ -3,5 +3,8 @@ package com.jupitters.bookinghotel.repository;
 import com.jupitters.bookinghotel.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    List<String> findDistinctRoomTypes();
 }
