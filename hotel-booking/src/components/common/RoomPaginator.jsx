838c2b7e45b70a8b150cd7f4fa52d80f964739b5
jupitters <sampaiobale@gmail.com>
2025-12-22 21:46:13 -0300

    room paginator component

--- hotel-booking/src/components/common/RoomPaginator.jsx
+++ hotel-booking/src/components/common/RoomPaginator.jsx
@@ -1,8 +1,17 @@
-const RoomPaginator = () => {
+const RoomPaginator = ({currentPage, totalPages, onPageChange}) => {
+    const pageNumbers = Array.from({length : totalPages}, (_, i) => i+1)
     return (
-        <div>
-
-        </div>
+        <nav>
+            <ul className="pagination, justify-content-center">
+                {pageNumbers.map((pageNumber) => (
+                    <li key={pageNumber} className={`page-item ${currentPage === pageNumber ? "active" : ""}`}>
+                        <button className="page-link" onClick={() => onPageChange(pageNumber)}>
+                            {pageNumber}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     )
 }
 
