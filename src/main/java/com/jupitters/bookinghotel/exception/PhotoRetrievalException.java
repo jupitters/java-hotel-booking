49b058c628eca1dd11ccd9e9a02869b4a5bfab92
jupitters <sampaiobale@gmail.com>
2025-12-22 21:26:31 -0300

    photo retrieval exception

--- src/main/java/com/jupitters/bookinghotel/exception/PhotoRetrievalException.java
+++ src/main/java/com/jupitters/bookinghotel/exception/PhotoRetrievalException.java
@@ -2,6 +2,6 @@ package com.jupitters.bookinghotel.exception;
 
 public class PhotoRetrievalException extends RuntimeException {
     public PhotoRetrievalException(String s) {
-        super(s)
+        super(s);
     }
 }
