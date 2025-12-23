3e2a8de7aaba078dbd9cbea283c598a1a74dd992
jupitters <sampaiobale@gmail.com>
2025-12-23 17:50:29 -0300

    existing room funcitons

--- hotel-booking/src/components/room/ExistingRooms.jsx
+++ hotel-booking/src/components/room/ExistingRooms.jsx
@@ -1,4 +1,5 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {getAllRooms} from "../utils/ApiFunctions.jsx";
 
 const ExistingRooms = () => {
     const [rooms, setRooms] = useState([])
@@ -9,6 +10,31 @@ const ExistingRooms = () => {
     const [selectedRoomType, setSelectedRoomType] = useState("")
     const [successMessage, setSuccessMessage] = useState("")
     const [errorMessage, setErrorMessage] = useState("")
+
+    useEffect(() => {
+        fetchRooms()
+    }, []);
+
+    const fetchRooms = async () => {
+        setIsLoading(true)
+        try{
+            const response = await getAllRooms()
+            setRooms(response)
+            setIsLoading(false)
+        } catch (error) {
+            setErrorMessage(error.message)
+        }
+    }
+
+    useEffect(() => {
+        if(selectedRoomType === ""){
+            setFilteredRooms(rooms)
+        }else {
+            const filtered = rooms.filter((room) => room.roomType === selectedRoomType)
+            setFilteredRooms(filtered)
+        }
+        setCurrentPage(1)
+    }, [rooms, selectedRoomType])
 
     return (
         <div>
