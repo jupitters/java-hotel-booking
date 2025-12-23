c220db5756caaa29024a38c413e2dc23afa9372d
jupitters <sampaiobale@gmail.com>
2025-12-22 21:53:53 -0300

    room filter component

--- hotel-booking/src/components/common/RoomFilter.jsx
+++ hotel-booking/src/components/common/RoomFilter.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const RoomFilter = () => {
-  return (
+const RoomFilter = ({data, setFilteredData}) => {
+    const [filter, setFilter] = useState("")
+
+    const handleSelectChange = (e) => {
+        const selectedRoomType = e.target.value
+        setFilter(selectedRoomType)
+        const filteredRooms = data.filter((room) => room.roomType.toLowerCase().includes(selectedRoomType.toLowerCase()))
+        setFilteredData(filteredRooms)
+    }
+
+    const clearFilter = () => {
+        setFilter("")
+        setFilteredData("")
+    }
+
+    return (
     <div>RoomFilter</div>
-  )
+    )
 }
 
 export default RoomFilter
