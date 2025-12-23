110e9857cb2b31375241538ccf12c216327c48c8
jupitters <sampaiobale@gmail.com>
2025-12-23 17:43:37 -0300

    existing room constants

--- hotel-booking/src/components/room/ExistingRooms.jsx
+++ hotel-booking/src/components/room/ExistingRooms.jsx
@@ -1,4 +1,15 @@
+import {useState} from "react";
+
 const ExistingRooms = () => {
+    const [rooms, setRooms] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+    const [roomsPerPage, setRoomsPerPage] = useState(8)
+    const [isLoading, setIsLoading] = useState(false)
+    const [filteredRooms, setFilteredRooms] = useState([])
+    const [selectedRoomType, setSelectedRoomType] = useState("")
+    const [successMessage, setSuccessMessage] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+
     return (
         <div>
 
