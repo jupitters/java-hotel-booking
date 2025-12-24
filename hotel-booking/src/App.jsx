bf70bc9aed571cb33eecd2ac0fe1f2c968f51d8b
jupitters <sampaiobale@gmail.com>
2025-12-24 19:22:46 -0300

    add routes

--- hotel-booking/src/App.jsx
+++ hotel-booking/src/App.jsx
@@ -5,6 +5,7 @@ import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import ExistingRooms from "./components/room/ExistingRooms.jsx";
 import Home from "./components/home/Home.jsx";
+import EditRoom from "./components/room/EditRoom.jsx";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/:roomId/view" element={<ViewRoom />} />
+                <Route path="/:roomId/view" element={<EditRoom />} />
             </Routes>
         </Router>
     </main>
