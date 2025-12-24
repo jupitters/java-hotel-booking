43ffbe6b6854d0439fd273b197db3df2b7d90305
jupitters <sampaiobale@gmail.com>
2025-12-24 19:29:12 -0300

    front: room update func

--- hotel-booking/src/components/utils/ApiFunctions.jsx
+++ hotel-booking/src/components/utils/ApiFunctions.jsx
@@ -46,3 +46,14 @@ export async function deleteRoom(roomId) {
         throw new Error(`Error deleting room: ${error.message}`)
     }
 }
+
+export async function updateRoom(roomId, roomData) {
+    const formData = new FormData()
+    formData.append("roomType", roomData.roomType)
+    formData.append("roomPrice", roomData.roomPrice)
+    formData.append("photo", roomData.photo)
+
+    const response = api.put(`/room/${roomId}/update`)
+    return response
+
+}
