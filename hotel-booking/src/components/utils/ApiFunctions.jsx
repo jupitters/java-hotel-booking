0073df965b8273b7b868f33aaca92146c85ccceb
jupitters <sampaiobale@gmail.com>
2025-12-25 13:56:27 -0300

    api functions

--- hotel-booking/src/components/utils/ApiFunctions.jsx
+++ hotel-booking/src/components/utils/ApiFunctions.jsx
@@ -4,20 +4,6 @@ export const api = axios.create({
     baseURL: "http://localhost:9192/api/v1"
 })
 
-export async function addRoom(photo, roomType, roomPrice){
-    const formData = new FormData()
-
-    formData.append("photo", photo)
-    formData.append("roomType", roomType)
-    formData.append("roomPrice", roomPrice)
-
-    const response = await api.post("/room/add", formData)
-    if(response.status === 201){
-        return true
-    } else {
-        return false
-    }
-}
 
 export async function getRoomTypes(){
     try{
