653ff5ee987da8261202a412bafae23331c4f991
jupitters <sampaiobale@gmail.com>
2025-12-26 09:08:37 -0300

    edit room

--- hotel-booking/src/components/room/EditRoom.jsx
+++ hotel-booking/src/components/room/EditRoom.jsx
@@ -1,4 +1,14 @@
 const EditRoom = () => {
+    const [newRoom, setNewRoom] = useState({
+        photo: null,
+        roomType: "",
+        roomPrice: ""
+    })
+
+    const [successMessage, setSuccessMessage] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+    const [imagePreview, setImagePreview] = useState("")
+
     return (
         <>
         </>
