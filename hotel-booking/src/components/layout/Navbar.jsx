f3fc375c290a8847b5be08fd98de958a42e9ac23
jupitters <sampaiobale@gmail.com>
2025-12-26 11:00:56 -0300

    navbar component

--- hotel-booking/src/components/layout/Navbar.jsx
+++ hotel-booking/src/components/layout/Navbar.jsx
@@ -1,7 +1,28 @@
+import {Link} from "react-router-dom";
+import {NavLink} from "react-bootstrap";
+
 const Navbar = () => {
     return (
-        <>
-        </>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary px-5 shadow mt-5 sticky-top">
+            <div className="container-fluid">
+                <Link to={"/"}>
+                    <span className="hotel-color"> Hotel</span>
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarScroll">
+                    <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+                        <li className="nav-item">
+                            <NavLink classname="nav-link" aria-current="page" to={"/all-rooms"}>
+                                Browse all rooms
+                            </NavLink>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
     )
 }
 
