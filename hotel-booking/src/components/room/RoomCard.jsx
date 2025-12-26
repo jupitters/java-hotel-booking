088ba44fd37453d143cf9e72520056dca4a19b0c
jupitters <sampaiobale@gmail.com>
2025-12-26 12:36:05 -0300

    room card component

--- hotel-booking/src/components/room/RoomCard.jsx
+++ hotel-booking/src/components/room/RoomCard.jsx
@@ -1,7 +1,16 @@
+import {Card, Col} from "react-bootstrap";
+
 const RoomCard = ({room}) => {
     return (
-        <>
-        </>
+        <Col key={room.id} className="mb-4" xs={12}>
+            <Card>
+                <Card.Body className="d-flex flex-wrap align-items-center">
+                    <div className="flex-shrink-0 mr-3 mb-3 mb-md-0">
+                        <Card.Img variant="top" src={`data:image/png;base64, ${room.photo}`} alt="Room Photo" style={{width:"100%", maxWidth:"200px", height:"auto"}} />
+                    </div>
+                </Card.Body>
+            </Card>
+        </Col>
     )
 }
 
