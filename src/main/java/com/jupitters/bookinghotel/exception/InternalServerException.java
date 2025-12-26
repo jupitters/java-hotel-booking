d99f009ef17dfa8529b922c31f611753c4d0b023
jupitters <sampaiobale@gmail.com>
2025-12-26 09:55:56 -0300

    internal server exception

---
+++ src/main/java/com/jupitters/bookinghotel/exception/InternalServerException.java
@@ -0,0 +1,7 @@
+package com.jupitters.bookinghotel.exception;
+
+public class InternalServerException extends RuntimeException {
+    public InternalServerException(String s) {
+        super(s);
+    }
+}
