83b1b8a795d84f8635a4ccc684349296a287fc9d
jupitters <sampaiobale@gmail.com>
2025-12-26 09:30:28 -0300

    edit room

--- hotel-booking/src/components/room/EditRoom.jsx
+++ hotel-booking/src/components/room/EditRoom.jsx
@@ -1,6 +1,7 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {getRoomById, updateRoom} from "../utils/ApiFunctions.jsx";
 import {useParams} from "react-router-dom";
+import RoomTypeSelector from "../common/RoomTypeSelector.jsx";
 
 const EditRoom = () => {
     const [room, setRoom] = useState({
@@ -61,6 +62,43 @@ const EditRoom = () => {
 
     return (
         <>
+            <section className='container, mt-5 mb-5'>
+                <div className='row justify-content-center'>
+                    <div className='col-md-8 col-lg-6'>
+                        <h2 className='mt-5 mb-2'>Add New Room</h2>
+                        {successMessage && (
+                            <div className="alert alert-success fade show">{successMessage}</div>
+                        )}
+                        {errorMessage && (
+                            <div className="alert alert-danger fade show">{errorMessage}</div>
+                        )}
+                        <form onSubmit={handleSubmit}>
+                            <div className='mb-3'>
+                                <label htmlFor="roomType" className='form-label'>Room Type</label>
+                                <div>
+                                    <RoomTypeSelector handleRoomInputChange={handleRoomInputChange} newRoom={newRoom} />
+                                </div>
+                            </div>
+
+                            <div className='mb-3'>
+                                <label htmlFor="roomPrice" className='form-label'>Room Price</label>
+                                <input className='form-control' required id='roomPrice' name='roomPrice' value={newRoom.roomPrice} onChange={handleRoomInputChange} type='number' />
+                            </div>
+
+                            <div className='mb-3'>
+                                <label htmlFor="photo" className='form-label'>Room Photo</label>
+                                <input className='form-control' onChange={handleImageChange} name="photo" id="photo" type='file' />
+                                {imagePreview && (
+                                    <img src={imagePreview} alt='Preview Room Photo' style={{maxWidth: '400px', maxHeight: '400px'}} className='mb-3' />
+                                )}
+                            </div>
+                            <div className='d-grid d-md-flex mt-2'>
+                                <button className='btn btn-outline-primary ml-5'>Save Room</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
