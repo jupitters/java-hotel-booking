adb2c20110481dac847da4bc9508f8775460d95c
jupitters <sampaiobale@gmail.com>
2025-12-27 13:31:50 -0300

    parallax component

---
+++ hotel-booking/src/components/common/Parallax.jsx
@@ -0,0 +1,16 @@
+import {Container} from "react-bootstrap";
+
+const Parallax = () => {
+    return (
+        <div className="parallax mb-5">
+            <Container>
+                <div className="animated-text bounceIn">
+                    <h1>Welcome to <span className="hotel-color">Hotel</span></h1>
+                    <h3>We offer the best services for you</h3>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Parallax
