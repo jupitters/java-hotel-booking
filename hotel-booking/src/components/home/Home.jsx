18eb7a0a93f4fbfa0cf6ea743f2fad7474c6de52
jupitters <sampaiobale@gmail.com>
2025-12-27 14:25:48 -0300

    home

--- hotel-booking/src/components/home/Home.jsx
+++ hotel-booking/src/components/home/Home.jsx
@@ -1,13 +1,17 @@
 import MainHeader from "../layout/MainHeader.jsx";
 import HotelService from "../common/HotelService.jsx";
 import Parallax from "../common/Parallax.jsx";
+import RoomCard from "../room/RoomCard.jsx";
+import RoomCarousel from "../common/RoomCarousel.jsx";
 
 const Home = () => {
     return (
         <section>
             <MainHeader />
             <section className="container">
+                <RoomCarousel />
                 <Parallax />
+                <RoomCarousel />
                 <HotelService />
                 <Parallax />
             </section>
