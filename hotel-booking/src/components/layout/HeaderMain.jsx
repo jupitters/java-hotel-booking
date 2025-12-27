c05aa354f61e29be53e045930fb56c38a55d2c39
jupitters <sampaiobale@gmail.com>
2025-12-27 13:22:30 -0300

    header main

--- hotel-booking/src/components/layout/HeaderMain.jsx
+++ hotel-booking/src/components/layout/HeaderMain.jsx
@@ -1,7 +1,12 @@
 const HeaderMain = () => {
     return (
-        <>
-        </>
+        <header className="header-banner">
+            <div className="overlay"></div>
+            <div className="animated-texts overlay-content">
+                <h1>Welcome to <span className="hotel-color">Hotel</span></h1>
+                <h4>Experience the best hospitality in town</h4>
+            </div>
+        </header>
     )
 }
 
