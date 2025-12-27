18c9464398d06883a0d464c0bcfe4a4aee57ba05
jupitters <sampaiobale@gmail.com>
2025-12-27 14:16:34 -0300

    room carousel component

--- hotel-booking/src/components/common/RoomCarousel.jsx
+++ hotel-booking/src/components/common/RoomCarousel.jsx
@@ -1,5 +1,7 @@
 import {useEffect, useState} from "react";
 import {getAllRooms} from "../utils/ApiFunctions.jsx";
+import {Link} from "react-router-dom";
+import {Card, Carousel, Col, Container, Row} from "react-bootstrap";
 
 const RoomCarousel = () => {
     const [rooms, setRooms] = useState([])
@@ -25,8 +27,40 @@ const RoomCarousel = () => {
     }
 
     return (
-        <>
-        </>
+        <section className="bg-light mb-5 mt-5 shadow">
+            <Link to={"/brose-all-rooms"} className="hotel-color text-center">
+                Browse All Rooms
+            </Link>
+            <Container>
+                <Carousel indicators={false}>
+                    {[...Array(Math.ceil(rooms.length / 4))].map((_, index) => (
+                        <Carousel.Item key={index}>
+                            <Row>
+                                {rooms.slice(index * 4, index * 4 + 4).map((room) => (
+                                    <Col key={room.id} className="mb-4" xs={12} md={6} lg={3}>
+                                        <Card>
+                                            <Link to={`/${room.id}/book-room/`} >
+                                                <Card.Img variant="top" src={`data:image/png;base64, ${room.photo}`} alt="Room Photo" className="w-100" style={{height:"200px"}} />
+                                            </Link>
+                                            <Card.Body>
+                                                <Card.Title className="hotel-color">{room.roomType}</Card.Title>
+                                                <Card.Title className="room-price">${room.roomPrice}/night</Card.Title>
+
+                                                <div className="flex-shrink-0">
+                                                    <Link to={`/${room.id}/book-room`} className="btn btn-sm btn-hotel">
+                                                        View/Book Now
+                                                    </Link>
+                                                </div>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+            </Container>
+        </section>
     )
 }
 
