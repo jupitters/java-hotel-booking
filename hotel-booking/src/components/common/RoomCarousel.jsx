d528e0b111a03cae0ded98953a88ff3df72c8818
jupitters <sampaiobale@gmail.com>
2025-12-27 14:01:45 -0300

    room carousel component

--- hotel-booking/src/components/common/RoomCarousel.jsx
+++ hotel-booking/src/components/common/RoomCarousel.jsx
@@ -1,4 +1,29 @@
+import {useEffect, useState} from "react";
+import {getAllRooms} from "../utils/ApiFunctions.jsx";
+
 const RoomCarousel = () => {
+    const [rooms, setRooms] = useState([])
+    const [errorMessage, setErrorMessage] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        setIsLoading(true)
+        getAllRooms().then((data) => {
+            setRooms(data)
+            setIsLoading(false)
+        }).catch((error) => {
+            setErrorMessage(error.message)
+            setIsLoading(false)
+        })
+    }, []);
+
+    if(isLoading) {
+        return <div className="mt-5">Loading rooms...</div>
+    }
+    if(errorMessage){
+        return <div className="text-danger mb-5 mt-5">Error: {errorMessage}</div>
+    }
+
     return (
         <>
         </>
