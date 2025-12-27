1c78a9aa6655660b9184f0d4b171f94a57b25669
jupitters <sampaiobale@gmail.com>
2025-12-27 12:39:09 -0300

    room component

--- hotel-booking/src/components/room/Room.jsx
+++ hotel-booking/src/components/room/Room.jsx
@@ -1,4 +1,15 @@
+import {useState} from "react";
+
 const Room = () => {
+    const [data, setData] = useState([])
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [roomsPerPage, setRoomsPerPage] = useState(6)
+    const [filteredData, setFilteredData] = useState([])
+
+
+
     return (
         <>
         </>
