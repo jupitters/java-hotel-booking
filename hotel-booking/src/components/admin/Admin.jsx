2f598583a2c1cde2b0e267cad05c4bcd8b786960
jupitters <sampaiobale@gmail.com>
2025-12-27 13:15:42 -0300

    admin component route

--- hotel-booking/src/components/admin/Admin.jsx
+++ hotel-booking/src/components/admin/Admin.jsx
@@ -1,7 +1,12 @@
+import {Link} from "react-router-dom";
+
 const Admin = () => {
     return (
-        <>
-        </>
+        <div>
+            <h2>Welcome to Admin Panel</h2>
+            <hr />
+            <Link to={"/add-room"}>Manage Rooms</Link>
+        </div>
     )
 }
 
