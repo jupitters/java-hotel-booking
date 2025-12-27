7df83aae4ddd1826ff7c01721664de1686a83af2
jupitters <sampaiobale@gmail.com>
2025-12-27 12:58:48 -0300

    room listing component

--- hotel-booking/src/components/room/RoomListing.jsx
+++ hotel-booking/src/components/room/RoomListing.jsx
@@ -1,7 +1,10 @@
+import Room from "./Room.jsx";
+
 const RoomListing = () => {
     return (
-        <>
-        </>
+        <section className="bg-light p-2 mb-5 mt-5 shadow">
+            <Room />
+        </section>
     )
 }
 
