7fb4c09176bbc18374ffee783d56ba349fac6a3f
jupitters <sampaiobale@gmail.com>
2025-12-29 10:19:02 -0300

    booking summary component

--- hotel-booking/src/components/bookings/BookingSummary.jsx
+++ hotel-booking/src/components/bookings/BookingSummary.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import moment from 'moment'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const BookingSummary = () => {
+const BookingSummary = ({booking, payment, isFormValid, onConfirm}) => {
+    const checkInDate = moment(booking.checkInDate)
+    const checkOutDate = moment(booking.checkOutDate)
+    const numOfDays = checkOutDate.diff(checkInDate, "days")
+    const [isBookingConfirmed, setIsBookingConfirmed] = useState(false)
+    const [isProcessingPayment, setIsProcessingPayment] = useState(false)
+    const navigate = useNavigate()
+
+    const handleConfirmBooking = () => {
+        setIsProcessingPayment(true)
+        setTimeout(() => {
+            setIsProcessingPayment(false)
+            setIsBookingConfirmed(true)
+            onConfirm()
+        }, 3000)
+    }
+
+    useEffect(() => {
+        if(isBookingConfirmed){
+            navigate("/booking-success")
+        }
+    }, [isBookingConfirmed, navigate])
+
   return (
     <div>BookingSummary</div>
   )
